7d1b5240285b12c682056f6d3b41ea3ac38a3c36
Florian Hoss <mail@florianhoss.de>
2023-07-27 20:42:13 +0200

    Add svg class for icons

--- web/src/components/ui/NavLink.vue
+++ web/src/components/ui/NavLink.vue
@@ -8,8 +8,6 @@ defineProps<{
   icon: string;
   status?: string;
   active: boolean;
-  background?: 'bg-primary' | 'bg-secondary';
-  text?: 'text-primary-content' | 'text-secondary-content';
   smallHidden?: boolean;
 }>();
 </script>
@@ -18,11 +16,8 @@ defineProps<{
   <li :class="{ 'hidden lg:flex': smallHidden }">
     <RouterLink class="flex justify-between px-2" :class="{ active: active }" :to="link">
       <div class="flex items-center gap-2">
-        <div
-          class="w-10 h-10 p-2 rounded-full flex items-center justify-center shrink-0 text-lg"
-          :class="background ? background + ' ' + text : 'bg-base-100 text-base-content'"
-        >
-          <span class="w-full" v-html="icon"></span>
+        <div class="w-10 h-10 p-2 rounded-full flex items-center justify-center shrink-0 text-lg bg-base-100 text-base-content">
+          <span v-html="icon"></span>
         </div>
         <div class="grid grid-cols-1">
           <div class="text-lg truncate">{{ name }}</div>
