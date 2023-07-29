5947c2b9ef2c9242de1ae9d022f3234509f7d8b3
Florian Hoss <mail@florianhoss.de>
2023-07-30 00:33:31 +0200

    Cleanup

--- internal/controller/runs.go
+++ internal/controller/runs.go
@@ -6,11 +6,11 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
-// @Schemes
-// @Tags		jobs
-// @Produce	json
-// @Success	200	{array}	database.Run
-// @Router		/jobs/runs [get]
+//	@Schemes
+//	@Tags		jobs
+//	@Produce	json
+//	@Success	200	{array}	database.Run
+//	@Router		/jobs/runs [get]
 func (c *Controller) GetRuns(ctx echo.Context) error {
 	return ctx.JSON(http.StatusOK, c.service.GetRuns())
 }
