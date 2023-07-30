63f81dee5eca537fe52d9837db8ddb9f922bed38
Florian Hoss <mail@florianhoss.de>
2023-07-30 12:02:09 +0200

    Fix badge overflow

--- web/src/components/ui/BadgeList.vue
+++ web/src/components/ui/BadgeList.vue
@@ -3,7 +3,7 @@ defineProps<{ badges: {} }>();
 </script>
 
 <template>
-  <div class="badge whitespace-nowrap" v-for="(badge, key, counter) of badges" :key="key">
-    <span class="opacity-50 mr-2">{{ Object.keys(badges)[counter] }}</span> {{ badge || '-' }}
+  <div class="badge whitespace-nowrap min-w-0" v-for="(badge, key, counter) of badges" :key="key">
+    <span class="opacity-50 mr-2">{{ Object.keys(badges)[counter] }}</span> <span class="truncate">{{ badge || '-' }}</span>
   </div>
 </template>
