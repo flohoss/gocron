89d7b18c120f56c4cb45c1664272b2bf7d667303
Florian Hoss <mail@florianhoss.de>
2025-01-28 21:30:13 +0100

    Init the run btn in backend

--- assets/js/event.js
+++ assets/js/event.js
@@ -1,10 +1,18 @@
+const play = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.347a1.125 1.125 0 0 1 0 1.972l-11.54 6.347a1.125 1.125 0 0 1-1.667-.986V5.653Z"></path>
+            </svg>`;
+const spin = `<span class="loading loading-spinner"></span>`;
+
 document.addEventListener('alpine:init', () => {
-    Alpine.data('event', () => ({
-        runIcon: '',
-        idle: false,
+    Alpine.data('event', (idling = false) => ({
+        idle: idling,
+        runBtn: idling ? play : spin,
         SSE: null,
         data: null,
         init() {
+            this.$watch('idle', (value) => {
+                value ? (this.runBtn = play) : (this.runBtn = spin);
+            });
             this.SSE = new EventSource("/api/events?stream=status");
             this.SSE.onmessage = (event) => {
                 this.data = JSON.parse(event.data);
@@ -22,16 +30,6 @@ document.addEventListener('alpine:init', () => {
         },
         handleData() {
             this.idle = this.data.idle;
-            this.handleRunIcon();
-        },
-        handleRunIcon() {
-            if (this.idle) {
-                this.runIcon = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.347a1.125 1.125 0 0 1 0 1.972l-11.54 6.347a1.125 1.125 0 0 1-1.667-.986V5.653Z"></path>
-                                </svg>`;
-            } else {
-                this.runIcon = `<span class="loading loading-spinner"></span>`;
-            }
         },
         run() {
             fetch('/api/jobs', { method: 'POST' })
