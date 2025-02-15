d27798a2aefb6922f10e592137ea70a483356550
Florian Hoss <mail@florianhoss.de>
2025-02-15 21:25:36 +0100

    Add installed versions dialog

--- web/src/components/AppHeader.vue
+++ web/src/components/AppHeader.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
 import { useEventSource } from '@vueuse/core';
-import { onBeforeUnmount, watch } from 'vue';
+import { watch } from 'vue';
 import { BackendURL } from '../main';
 import { useEventStore } from '../stores/event';
-import { PlayIcon, ChevronLeftIcon } from '@heroicons/vue/24/outline';
+import { PlayIcon, ChevronLeftIcon, InformationCircleIcon } from '@heroicons/vue/24/outline';
 import { useRouter } from 'vue-router';
 import { JobsService } from '../openapi';
+import VersionDialog from './VersionDialog.vue';
 
 const router = useRouter();
 const store = useEventStore();
@@ -13,7 +14,9 @@ const store = useEventStore();
 const { data, close } = useEventSource(BackendURL + 'api/events?stream=status', [], {
   autoReconnect: true,
 });
-onBeforeUnmount(() => close());
+addEventListener('beforeunload', () => {
+  close();
+});
 watch(() => data.value, store.parseEventInfo);
 
 const run = async () => {
@@ -27,13 +30,19 @@ const run = async () => {
 
 <template>
   <header class="flex justify-between items-center md:justify-center md:gap-20 mb-4 md:mb-8">
-    <button @click="router.push('/')" class="btn btn-soft btn-circle">
+    <button v-if="$route.name !== 'homeView'" @click="router.push('/')" class="btn btn-soft btn-circle">
       <ChevronLeftIcon class="size-6" />
     </button>
+    <button v-else onclick="version_modal.showModal()" class="btn btn-soft btn-circle">
+      <InformationCircleIcon class="size-6" />
+    </button>
+
     <img class="size-28 lg:size-36" src="/static/logo.webp" />
     <button @click="run" class="btn btn-soft btn-circle" :disabled="!store.idle">
       <PlayIcon v-if="store.idle" class="size-6" />
       <span v-else class="loading loading-spinner"></span>
     </button>
+
+    <VersionDialog id="version_modal" />
   </header>
 </template>
