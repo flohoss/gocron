3f2b2412053ae398f3bf64c4d7bca556b5d6853b
Florian Hoss <mail@florianhoss.de>
2025-02-15 21:00:37 +0100

    Fix docker build

--- handlers/routes.go
+++ handlers/routes.go
@@ -7,10 +7,14 @@ import (
 	echoSwagger "github.com/swaggo/echo-swagger"
 )
 
-func SetupRoutes(e *echo.Echo, jh *JobHandler) {
-	e.GET("/", jh.listHandler)
-	e.GET("/:name", jh.jobHandler)
+func longCacheLifetime(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		c.Response().Header().Set(echo.HeaderCacheControl, "public, max-age=31536000")
+		return next(c)
+	}
+}
 
+func SetupRoutes(e *echo.Echo, jh *JobHandler) {
 	api := e.Group("/api")
 	api.GET("/events", jh.JobService.GetHandler())
 
@@ -20,12 +24,19 @@ func SetupRoutes(e *echo.Echo, jh *JobHandler) {
 	jobs.POST("", jh.executeJobsHandler)
 	jobs.POST("/:name", jh.executeJobHandler)
 
-	e.GET("/swagger/*", echoSwagger.WrapHandler)
+	api.GET("/swagger/*", echoSwagger.WrapHandler)
 
 	e.GET("/robots.txt", func(ctx echo.Context) error {
 		return ctx.String(http.StatusOK, "User-agent: *\nDisallow: /")
 	})
-	e.RouteNotFound("/*", func(c echo.Context) error {
-		return c.Redirect(http.StatusTemporaryRedirect, "/")
+
+	assets := e.Group("/assets", longCacheLifetime)
+	assets.Static("/", "web/assets")
+
+	favicon := e.Group("/static", longCacheLifetime)
+	favicon.Static("/", "web/static")
+
+	e.RouteNotFound("*", func(ctx echo.Context) error {
+		return ctx.Render(http.StatusOK, "index.html", nil)
 	})
 }
