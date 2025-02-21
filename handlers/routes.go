104e60d084b647c05baaefa77abfc92d1369b5c5
Florian Hoss <mail@florianhoss.de>
2025-02-21 15:25:42 +0100

    Change margin

--- handlers/routes.go
+++ handlers/routes.go
@@ -24,8 +24,23 @@ func SetupRouter(jh *JobHandler) *echo.Echo {
 	config := huma.DefaultConfig("No Backup No Mercy API", "1.0.0")
 	config.OpenAPIPath = "/api/openapi"
 	config.SchemasPath = "/api/schemas"
-	config.DocsPath = "/api/docs"
 	h := humaecho.New(e, config)
+
+	e.GET("/api/docs", func(ctx echo.Context) error {
+		return ctx.HTML(http.StatusOK, `<!doctype html>
+			<html>
+				<head>
+					<title>API Reference</title>
+					<meta charset="utf-8" />
+					<meta name="viewport" content="width=device-width, initial-scale=1" />
+				</head>
+				<body>
+					<script id="api-reference" data-url="/api/openapi.json"></script>
+					<script src="https://cdn.jsdelivr.net/npm/@scalar/api-reference"></script>
+				</body>
+			</html>`,
+		)
+	})
 
 	e.Use(echo.WrapMiddleware(chimiddleware.Heartbeat("/api/health")))
 	e.Use(middleware.Recover())
