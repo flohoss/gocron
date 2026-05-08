326cf9e0d0cfc469ff18128ce40f3bea32910c23
Florian Hoss <mail@florianhoss.de>
2026-05-08 20:20:05 +0200

    Add Cypress tests for GUI navigation & modals
    Expand e2e coverage in web/e2e/cypress/e2e/gui.cy.js by adding tests that: navigate to the Commands view and back, verify the OpenAPI docs link, open and close the Select Jobs modal (including search input), and validate rejection messages for a disallowed terminal command and for disallowed arguments on an allowed command. These tests improve UI regression detection for navigation, modals, and command validation behavior.

--- web/e2e/cypress/e2e/gui.cy.js
+++ web/e2e/cypress/e2e/gui.cy.js
@@ -41,4 +41,49 @@ describe('should render the GUI', () => {
       cy.get('[data-test-id="job-link"]').should('be.visible');
     });
   });
+
+  it('should navigate to command view and back', () => {
+    cy.visit('/');
+
+    cy.get('[data-test-id="terminal-button"]').click();
+    cy.url().should('include', '/commands');
+
+    cy.get('[data-test-id="back-button"]').should('be.visible').click();
+    cy.url().should('match', /\/$/);
+    cy.get('[data-test-id="terminal-button"]').should('be.visible');
+  });
+
+  it('should expose the openapi docs link', () => {
+    cy.visit('/');
+
+    cy.get('[data-test-id="openapi-button"] a').should('have.attr', 'href', '/api/docs');
+  });
+
+  it('should open and close the select jobs modal', () => {
+    cy.visit('/');
+
+    cy.get('[data-test-id="select-button"] button').click();
+    cy.get('#selectModal').should('have.attr', 'open');
+    cy.contains('h3', 'Select Jobs').should('be.visible');
+    cy.get('input[type="search"]').should('be.visible').type('job');
+
+    cy.get('form.modal-backdrop button').click({ force: true });
+    cy.get('#selectModal').should('not.have.attr', 'open');
+  });
+
+  it('should reject a disallowed terminal command', () => {
+    cy.visit('/commands');
+
+    cy.get('input[placeholder="Command"]').type('whoami{enter}');
+    cy.contains('code', 'Executing command: whoami').should('be.visible');
+    cy.contains('code', 'command "whoami" is not allowed').should('be.visible');
+  });
+
+  it('should reject disallowed arguments for allowed command', () => {
+    cy.visit('/commands');
+
+    cy.get('input[placeholder="Command"]').type('docker images{enter}');
+    cy.contains('code', 'Executing command: docker images').should('be.visible');
+    cy.contains('code', 'argument "images" is not allowed for command "docker"').should('be.visible');
+  });
 });
